cd73bad67f85fd06205f57a6ce9af678e1e23e14
Cans Fong <cans_fong@163.com>
2018-05-12 00:35:22 +0800

    add exception handles

--- utility/big_file_locator.go
+++ utility/big_file_locator.go
@@ -14,14 +14,23 @@ import (
 
 // 返回文件或目录（递归统计）大小，单位 byte
 func getSize(file string) int64 {
-	f, _ := os.Stat(file)
 	var size_b int64
+	f, err := os.Stat(file)
 
+	if err != nil {
+		return 0
+	}
+	
 	if f.IsDir() == false {
 		size_b = f.Size()
 	} else {
-		filepath.Walk(file, func(path string, info os.FileInfo, err error) error {
-			size_b += info.Size()
+		filepath.Walk(file, func(file string, info os.FileInfo, err error) error {
+			if err == nil {
+				size_b += info.Size()
+			} else {
+				return filepath.SkipDir
+			}
+			
 			return nil
 		})
 	}
@@ -33,7 +42,12 @@ func getSize(file string) int64 {
 func getFiles(dir string) []string {
 	var files []string
 	cmd := exec.Command("ls", dir)
-	out, _ := cmd.CombinedOutput()
+	out, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return nil
+	}
+
 	tmp := strings.Split(strings.TrimRight(string(out), "\n"), "\n")
 
 	for _, file := range tmp {
@@ -45,6 +59,7 @@ func getFiles(dir string) []string {
 
 func locator(dir string, m map[string]int64) string {
 	flg := "N"
+
 	for _, file := range getFiles(dir) {
 		f, _ := os.Stat(file)
 		pct := getSize(file) * 100 / total
