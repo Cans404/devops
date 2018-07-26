fa2de67871c666a8eff8a1b5675dd9c28092a5dd
Cans Fong <cans_fong@163.com>
2018-07-26 22:39:32 +0800

    add orcl temp tablespace wait event self healthing

---
+++ monitor/tmp_wait_healthing.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# created by cans, 20180726
+
+source ~/.bash_profile
+cd /home/oracle/script
+
+declare -A tmp_wait
+
+declare `sqlplus -S '/as sysdba' << EOF | awk '{print "tmp_wait["$1"]="$2}'
+set feedback off
+set heading off
+set newpage none
+select t2.name, count(t1.event) num
+  from gv$session t1, sys.ts$ t2, dba_tablespaces t3
+where t1.event = 'enq: TS - contention'
+ and t1.p2 = t2.ts#
+ and t2.name = t3.tablespace_name
+ and t3.contents = 'TEMPORARY'
+group by t2.name
+having count(t1.event) > 50;
+exit
+EOF`
+
+function add_tmpfile {
+	sqlplus -S '/as sysdba' << EOF
+	set newpage none
+	set feedback off
+	set echo off
+	alter tablespace $1 add tempfile '+DATA01' size 32868M autoextend off;
+	alter tablespace $1 add tempfile '+DATA01' size 32868M autoextend off;
+	EOF
+}
+
+if (( ${#tmp_wait[*]} > 0 )); then
+	message="### orcl alert on ${ORACLE_SID:0:5} ###\n"
+
+	for key in ${!tmp_wait[*]}
+	do
+		message=${message}"wait event for $key up to ${tmp_wait[$key]}\n"
+		add_tmpfile $key
+	done
+
+	/home/oracle/script/bidwsns-bin -tel "186****6197" -msg "`echo -e ${message}`"
+fi
